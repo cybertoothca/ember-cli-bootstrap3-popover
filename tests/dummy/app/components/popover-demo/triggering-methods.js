d7cb3d136c949065cfbfc1f55bcdd2c0b6764ed5
Dan Nelson <dnelson@cybertooth.ca>
2016-12-09 00:48:44 -0700

    #4 #11 Improving events demo.

--- tests/dummy/app/components/popover-demo/triggering-methods.js
+++ tests/dummy/app/components/popover-demo/triggering-methods.js
@@ -6,36 +6,41 @@ export default Ember.Component.extend({
     onShow() {
       this.$('.label-show')
         .addClass('label-success');
-      setInterval(function () {
-        this.$('.label-show').removeClass('label-success');
+      setTimeout(function () {
+        this.$('.label-show')
+          .removeClass('label-success');
       }, 2500);
     },
     onShown() {
       this.$('.label-shown')
         .addClass('label-success');
-      setInterval(function () {
-        this.$('.label-shown').removeClass('label-success');
+      setTimeout(function () {
+        this.$('.label-shown')
+          .removeClass('label-success');
       }, 2500);
     },
     onHide() {
       this.$('.label-hide')
         .addClass('label-success');
-      setInterval(function () {
-        this.$('.label-hide').removeClass('label-success');
+      setTimeout(function () {
+        this.$('.label-hide')
+          .removeClass('label-success');
       }, 2500);
     },
     onHidden() {
       this.$('.label-hidden')
         .addClass('label-success');
-      setInterval(function () {
-        this.$('.label-hidden').removeClass('label-success');
+      setTimeout(function () {
+        this.$('.label-hidden')
+          .removeClass('label-success');
       }, 2500);
     },
     onInserted() {
       this.$('.label-inserted')
         .addClass('label-success');
-      setInterval(function () {
-        this.$('.label-inserted').removeClass('label-success');
+      setTimeout(function () {
+        this.$('.label-inserted')
+          .removeClass('label-success');
       }, 2500);
     }
   },
