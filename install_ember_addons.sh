3dcf24d4218563b4e17e91e200631ddd9df38f83
Dan Nelson <dnelson@cybertooth.ca>
2017-08-23 10:08:13 -0600

    Upgrade to Ember-2.14.

--- install_ember_addons.sh
+++ install_ember_addons.sh
@@ -6,5 +6,6 @@ ember install ember-cli-deploy-build
 ember install ember-cli-deploy-gzip
 ember install ember-cli-deploy-s3
 ember install ember-cli-font-awesome-sass
+ember install ember-clock
 
 npm install --save-dev bootswatch@3.3
