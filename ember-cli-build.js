9c55b9abc060ac1c5e34cc2d2b4fb40e5c68cf2b
nadnoslen <dan@cybertooth.io>
2020-01-10 18:02:15 -0700

    Eliminate bower.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,12 +7,44 @@ module.exports = function(defaults) {
     // Add options here
   });
 
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
+  // bootstrap & bootstrap-sass from node_modules
+  app.import("node_modules/bootstrap/dist/js/bootstrap.js");
+  app.import("node_modules/bootstrap/fonts/glyphicons-halflings-regular.eot", {
+    destDir: "assets/bootstrap"
+  });
+  app.import("node_modules/bootstrap/fonts/glyphicons-halflings-regular.svg", {
+    destDir: "assets/bootstrap"
+  });
+  app.import("node_modules/bootstrap/fonts/glyphicons-halflings-regular.ttf", {
+    destDir: "assets/bootstrap"
+  });
+  app.import("node_modules/bootstrap/fonts/glyphicons-halflings-regular.woff", {
+    destDir: "assets/bootstrap"
+  });
+  app.import(
+    "node_modules/bootstrap/fonts/glyphicons-halflings-regular.woff2",
+    { destDir: "assets/bootstrap" }
+  );
+
+  // font-awesome from node_modules
+  app.import("node_modules/font-awesome/fonts/FontAwesome.otf", {
+    destDir: "assets/font-awesome/fonts"
+  });
+  app.import("node_modules/font-awesome/fonts/fontawesome-webfont.eot", {
+    destDir: "assets/font-awesome/fonts"
+  });
+  app.import("node_modules/font-awesome/fonts/fontawesome-webfont.svg", {
+    destDir: "assets/font-awesome/fonts"
+  });
+  app.import("node_modules/font-awesome/fonts/fontawesome-webfont.ttf", {
+    destDir: "assets/font-awesome/fonts"
+  });
+  app.import("node_modules/font-awesome/fonts/fontawesome-webfont.woff", {
+    destDir: "assets/font-awesome/fonts"
+  });
+  app.import("node_modules/font-awesome/fonts/fontawesome-webfont.woff2", {
+    destDir: "assets/font-awesome/fonts"
+  });
 
   return app.toTree();
 };
